7b8430d9cb981b9ed9b9d26c666222d569f2de4e
zs <zs@bjpowernode.com>
2021-05-19 12:37:35 +0800

    123456

--- src/com/bjpowernode/domain/Person.java
+++ src/com/bjpowernode/domain/Person.java
@@ -2,5 +2,13 @@ package com.bjpowernode.domain;
 
 public class Person {
     private String id;
+    private String name;
 
+    public Person() {
+    }
+
+    public Person(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
